6159b26d66367a223cf32f422d353486e416911b
Dakshina D <Dakshina.D@ibm.com>
2024-01-24 11:10:07 +0530

    Using state hook

--- src/Title.tsx
+++ src/Title.tsx
@@ -1,9 +1,23 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import "./App.css";
-const Title = (): ReactElement => (
-  <div className="App">
-    <h1>Hello</h1>
-    <button>CLICK ME</button>
-  </div>
-);
+
+const Title = (): ReactElement => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const buttonClick = () => {
+    setIsVisible(!isVisible);
+  };
+  return (
+    <div className="App">
+      {isVisible && <h1>Hello</h1>}
+      <button onClick={buttonClick}>Click Me</button>
+    </div>
+  );
+};
+
 export default Title;
+
+// {/* <div className="App">
+//     <h1>Hello</h1>
+//     <button onClick={}>CLICK ME</button>
+//   </div> */}
